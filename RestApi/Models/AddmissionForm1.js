39273b91cce230259243ae624a2f8ede0bf3953b
Tehniyat-Khatoon <134380951+Tehniyat-Khatoon@users.noreply.github.com>
2023-10-10 21:02:53 +0530

    Type

--- RestApi/Models/AddmissionForm1.js
+++ RestApi/Models/AddmissionForm1.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const AddmissionForm1Schema = new mongoose.Schema ( {
 
     name:{type:String,required:true,trim:true},
-    phone:{type:mongoose.Decimal128,min:10,max:12,require:true},
+    phone:{type:Number,min:10,max:12,require:true},
     email:{type:String,require:true},
     course1:{type:String,require:true},
     course2:{type:String,require:true},
@@ -16,8 +16,8 @@ const AddmissionForm1Schema = new mongoose.Schema ( {
     subject5:{type:String,require:true},
     subject6:{type:String,require:true},
     image:{type:String,require:true},
-    installmentInput1:{type:mongoose.Decimal128,min:10,max:11,require:true},
-    installmentInput2:{type:mongoose.Decimal128,min:10,max:11,require:true},
+    installmentInput1:{type:Number,min:10,max:11,require:true},
+    installmentInput2:{type:Number,min:10,max:11,require:true},
     installment1Date1:{type:Date,require:true},
     installment1Date2:{type:Date,require:true},
     address:{type:String,require:true},
